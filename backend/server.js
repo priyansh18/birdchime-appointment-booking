92bcc70be2c21725f57a818b4438c7231251d431
Priyansh Singhal <18_priyansh@Priyanshs-MacBook-Air.local>
2025-11-06 20:25:40 +0530

    chore: added server cleanup, vercel deployment setup

--- backend/server.js
+++ backend/server.js
@@ -4,29 +4,31 @@ const cors = require('cors');
 const app = express();
 let inMemoryData = [];
 
-// Uncomment it out if in development mode
-// app.use(morgan("tiny"));
-var whitelist = [
-  "https://babfrontend.vercel.app/",
-  "https://babfrontend.vercel.app",
-  "http://localhost:5173", // for react apps
-  "http://localhost:3000", // for react apps
-  "http://localhost:4000", // for react apps
-];
+// // Uncomment it out if in development mode
+// // app.use(morgan("tiny"));
+// var whitelist = [
+//   "https://babfrontend.vercel.app/",
+//   "https://babfrontend.vercel.app",
+//   "http://localhost:5173", // for react apps
+//   "http://localhost:3000", // for react apps
+//   "http://localhost:4000", // for react apps
+// ];
 
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
+
+// Enable CORS for all routes by using the middleware globally
+app.use(cors());
 
 // Routes
 app.get('/', (req, res) => {
